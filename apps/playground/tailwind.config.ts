4e513d5037009eabb06803635d7795529f017a28
Harsh Mathur <harshmathurx@gmail.com>
2026-02-16 00:59:57 +0530

    chore: optimize build

--- apps/playground/tailwind.config.ts
+++ apps/playground/tailwind.config.ts
@@ -5,7 +5,8 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "../../patterns/**/*.{js,ts,jsx,tsx}",
+    "../../patterns/*/component.tsx",
+    "../../patterns/*/example.tsx",
   ],
   theme: {
     extend: {
